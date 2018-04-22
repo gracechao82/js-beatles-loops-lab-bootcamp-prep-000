d4826a69c593dba6b815256a3693f62f8e909382
gracey12051@gmail.com <gracey12051@gmail.com>
2018-04-22 20:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ const facts = ["He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"]
-let i = facts.length
-while 
+let i = 0
+while (i < facts.length){
+  facts[i] = facts[i] + "!!!";
+  i++
+}
