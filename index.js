5de41b6307bbb81c5673c9eb0cb8dd2083f9dd67
gracey12051@gmail.com <gracey12051@gmail.com>
2018-04-22 20:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ const facts = ["He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"]
+
+function johnLennonFacts(facts) {
 let i = 0
 while (i < facts.length){
   facts[i] = facts[i] + "!!!";
   i++
+}
+return facts
 }
